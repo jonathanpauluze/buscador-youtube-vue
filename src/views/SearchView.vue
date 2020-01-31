73fa7efe0d8d1ca54368616309465e2f070787b1
unknown <ATENDIMENTO@atendimento03.chinatown.local>
2020-01-31 13:17:54 -0300

    add: modal de detalhes

--- src/views/SearchView.vue
+++ src/views/SearchView.vue
@@ -2,18 +2,21 @@
   <v-app>
     <SearchForm />
     <VideoList />
+    <DetailsModal />
   </v-app>
 </template>
 
 <script>
 import SearchForm from '@/components/SearchForm';
 import VideoList from '@/components/VideoList';
+import DetailsModal from '@/components/DetailsModal';
 
 export default {
   name: 'App',
   components: {
     SearchForm,
-    VideoList
+    VideoList,
+    DetailsModal
   }
 };
 </script>
